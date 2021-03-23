31adda26948ed773837cd25e2f333a0aff4cedcd
loveefraimsson <loveefraimsson@gmail.com>
2021-03-23 09:28:00 +0100

    Second exercise done

--- script.js
+++ script.js
@@ -5,7 +5,25 @@
 // console.log(cars[2]);
 // console.log(cars[3]);
 
+// let cars = ['volvo', 'saab', 'bmw', 'tesla'];
+// for (car in cars) {
+//     console.log(cars[car]);
+// }
+
+//KOD 2---------------------------------------------------------------
+// let cars = ['volvo', 'saab', 'bmw', 'tesla'];
+// let fruits = ['apple', 'banana', 'orange'];
+// console.log(cars[0]);
+// console.log(cars[1]);
+// console.log(cars[2]);
+// console.log(cars[3]);
+
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+
 let cars = ['volvo', 'saab', 'bmw', 'tesla'];
-for (car in cars) {
-    console.log(cars[car]);
-}
+let fruits = ['apple', 'banana', 'orange'];
+
+cars.forEach(car => console.log(car));
+fruits.forEach(fruit => console.log(fruit));
