2dee19783d1376e774b88fad079047ac8791aa7f
loveefraimsson <loveefraimsson@gmail.com>
2021-03-23 09:17:05 +0100

    First exercise done

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("HEJ");
+//KOD 1-------------------------------------------------------------
+// let cars = ['volvo', 'saab', 'bmw', 'tesla'];
+// console.log(cars[0]);
+// console.log(cars[1]);
+// console.log(cars[2]);
+// console.log(cars[3]);
+
+let cars = ['volvo', 'saab', 'bmw', 'tesla'];
+for (car in cars) {
+    console.log(cars[car]);
+}
