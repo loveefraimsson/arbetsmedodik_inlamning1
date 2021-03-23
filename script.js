e9d20e04f3a77d77bb383f007050fe788cf22594
loveefraimsson <loveefraimsson@gmail.com>
2021-03-23 09:32:59 +0100

    Third exercise done

--- script.js
+++ script.js
@@ -22,8 +22,27 @@
 // console.log(fruits[1]);
 // console.log(fruits[2]);
 
-let cars = ['volvo', 'saab', 'bmw', 'tesla'];
-let fruits = ['apple', 'banana', 'orange'];
+// let cars = ['volvo', 'saab', 'bmw', 'tesla'];
+// let fruits = ['apple', 'banana', 'orange'];
 
-cars.forEach(car => console.log(car));
-fruits.forEach(fruit => console.log(fruit));
+// cars.forEach(car => console.log(car));
+// fruits.forEach(fruit => console.log(fruit));
+
+//KOD 3 --------------------------------------------------------------
+// let x = ['rose', 'tulip', 'lily', 'orchid'];
+
+// function asdasd(banana) {
+//     for (let i = 0; i < banana.length; i++) { 
+//         console.log(banana[i]);
+//     }
+// }
+// asdasd(x);
+
+let flowers = ['rose', 'tulip', 'lily', 'orchid'];
+
+function printFlowers(flowers) {
+    for (let i = 0; i < flowers.length; i++) { 
+        console.log(flowers[i]);
+    }
+}
+printFlowers(flowers);
