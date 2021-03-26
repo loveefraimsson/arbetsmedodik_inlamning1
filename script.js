99a90637579035b505bcbcbf1eed523cdf59103e
loveefraimsson <loveefraimsson@gmail.com>
2021-03-26 11:22:59 +0100

    Finished assignment

--- script.js
+++ script.js
@@ -1,43 +1,17 @@
 //KOD 1-------------------------------------------------------------
-// let cars = ['volvo', 'saab', 'bmw', 'tesla'];
-// console.log(cars[0]);
-// console.log(cars[1]);
-// console.log(cars[2]);
-// console.log(cars[3]);
-
-// let cars = ['volvo', 'saab', 'bmw', 'tesla'];
-// for (car in cars) {
-//     console.log(cars[car]);
-// }
+let cars = ['volvo', 'saab', 'bmw', 'tesla'];
+for (car in cars) {
+    console.log(cars[car]);
+}
 
 //KOD 2---------------------------------------------------------------
-// let cars = ['volvo', 'saab', 'bmw', 'tesla'];
-// let fruits = ['apple', 'banana', 'orange'];
-// console.log(cars[0]);
-// console.log(cars[1]);
-// console.log(cars[2]);
-// console.log(cars[3]);
+let cars = ['volvo', 'saab', 'bmw', 'tesla'];
+let fruits = ['apple', 'banana', 'orange'];
 
-// console.log(fruits[0]);
-// console.log(fruits[1]);
-// console.log(fruits[2]);
+cars.forEach(car => console.log(car));
+fruits.forEach(fruit => console.log(fruit));
 
-// let cars = ['volvo', 'saab', 'bmw', 'tesla'];
-// let fruits = ['apple', 'banana', 'orange'];
-
-// cars.forEach(car => console.log(car));
-// fruits.forEach(fruit => console.log(fruit));
-
-//KOD 3 --------------------------------------------------------------
-// let x = ['rose', 'tulip', 'lily', 'orchid'];
-
-// function asdasd(banana) {
-//     for (let i = 0; i < banana.length; i++) { 
-//         console.log(banana[i]);
-//     }
-// }
-// asdasd(x);
-
+// //KOD 3 --------------------------------------------------------------
 let flowers = ['rose', 'tulip', 'lily', 'orchid'];
 
 function printFlowers(flowers) {
